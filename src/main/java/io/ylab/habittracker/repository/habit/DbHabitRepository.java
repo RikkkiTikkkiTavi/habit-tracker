07ca2cbffc6899e98af58c34302d2f6c6d226005
Constantin Scheglov <Rikkki.Tikkki.Tavi@yandex.ru>
2024-10-21 13:07:57 +0300

    add javadoc

--- src/main/java/io/ylab/habittracker/repository/habit/DbHabitRepository.java
+++ src/main/java/io/ylab/habittracker/repository/habit/DbHabitRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 /**
- * Класс утилита для работы с конфигом
+ * Класс для работы с базой данных
  *
  * @autor Константин Щеглов
  */
