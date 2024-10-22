f6039e67fcbecde4304590c2521e144484dc990d
Constantin Scheglov <Rikkki.Tikkki.Tavi@yandex.ru>
2024-10-23 00:02:37 +0300

    update: new streak detection

--- src/main/java/io/ylab/habittracker/service/history/HistoryServiceImpl.java
+++ src/main/java/io/ylab/habittracker/service/history/HistoryServiceImpl.java
@@ -1,9 +1,14 @@
 package io.ylab.habittracker.service.history;
 
+import io.ylab.habittracker.model.habit.Frequency;
+import io.ylab.habittracker.model.habit.Habit;
 import io.ylab.habittracker.model.history.HabitHistory;
+import io.ylab.habittracker.repository.habit.HabitRepoManager;
+import io.ylab.habittracker.repository.habit.HabitRepository;
 import io.ylab.habittracker.repository.history.HistoryManager;
 import io.ylab.habittracker.repository.history.HistoryRepository;
 import io.ylab.habittracker.validate.Validate;
+import io.ylab.habittracker.validate.ValidationException;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -12,16 +17,39 @@ import java.util.*;
 public class HistoryServiceImpl implements HistoryService {
 
     private final HistoryRepository hr = HistoryManager.getInstance();
+    private final HabitRepository habitRepository = HabitRepoManager.getInstance();
 
     @Override
     public void noteHabit(long userId, long habitId) {
+        Habit habit = habitRepository.getHabit(habitId);
+
+        if (habit == null) {
+            throw new ValidationException("Привычка не найдена");
+        }
+
         HabitHistory habitHistory = new HabitHistory(habitId, userId, LocalDate.now());
-        HabitHistory previousHistory = hr.getHistory(habitId, LocalDate.now().minusDays(1));
-        if (previousHistory == null) {
-            hr.addUserHistory(habitHistory);
+
+        HabitHistory previousHistory =
+                hr.getHistory(habitId, LocalDate.now().minusDays(1));
+
+        List<HabitHistory> previousWeekHistory =
+                hr.getUserHistory(userId, LocalDate.now().minusDays(7), LocalDate.now());
+
+        if (habit.getFrequency().equals(Frequency.EVERY_DAY)) {
+            if (previousHistory == null) {
+                hr.addUserHistory(habitHistory);
+            } else {
+                habitHistory.setStreak(previousHistory.getStreak() + 1);
+                hr.addUserHistory(habitHistory);
+            }
         } else {
-            habitHistory.setStreak(previousHistory.getStreak() + 1);
-            hr.addUserHistory(habitHistory);
+            if (previousWeekHistory.isEmpty()) {
+                hr.addUserHistory(habitHistory);
+            } else {
+                int size = previousWeekHistory.size();
+                habitHistory.setStreak(previousWeekHistory.get(size-1).getStreak() + 1);
+                hr.addUserHistory(habitHistory);
+            }
         }
     }
 
@@ -50,8 +78,6 @@ public class HistoryServiceImpl implements HistoryService {
         if (lastDate != null) {
             if (lastDate.equals(LocalDate.now().minusDays(1)) || lastDate.equals(LocalDate.now())) {
                 streak = hr.getHistory(habitId, lastDate).getStreak();
-            } else {
-                streak = 1;
             }
         }
         return streak;
